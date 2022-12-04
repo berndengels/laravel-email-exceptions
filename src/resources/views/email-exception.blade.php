9af715e83c754f8e9ee3c7d07cd646b0982f9ff7
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-12-04 18:28:57 +0100

    fix view

--- src/resources/views/email-exception.blade.php
+++ src/resources/views/email-exception.blade.php
@@ -5,87 +5,90 @@
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
 <body style="color: #000000; font-family: 'Open Sans', sans-serif;">
-
-<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
-    <tr>
-        <td align="center" valign="top">
-            <table border="0" cellpadding="20" cellspacing="0" width="600" id="emailContainer">
-                <tr>
-                    <td align="center" valign="top">
-                        <h3 style="height: 40px; line-height: 40px; background-color: #f56857; color: #ffffff;">There has been an exception thrown on {{ env('APP_URL', 'unknown') }}</h3>
-                        <table class="emailExceptionTable" style="text-align: left;" border="0" cellspacing="0" cellpadding="3">
-                            <tr>
-                                <td><strong>Environment:</strong></td>
-                                <td>{{ env('APP_ENV', 'unknown') }}</td>
-                            </tr>
-                        @if($request)
-                            <tr>
-                                <td><strong>Exception Url:</strong></td>
-                                <td>{!! $request->fullUrl() ?? null !!}</td>
-                            </tr>
-                            <tr>
-                                <td><strong>Remote Host:</strong></td>
-                                <td>{!! $request->ip() ?? null !!}</td>
-                            </tr>
-
-                            @if($request->input())
-                            <tr>
-                                <td><strong>Request:</strong></td>
-                                <td><pre>{!! print_r($request->input()) !!}</pre></td>
-                            </tr>
-                            @endif
-                        @endif
-
-                        @if($agent)
-                            <tr>
-                                <td><strong>UserAgent:</strong></td>
-                                <td>{!! $agent->getUserAgent() !!}</td>
-                            </tr>
-                            @if($agent->isRobot())
+@isset($request)
+    <table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="bodyTable">
+        <tr>
+            <td align="center" valign="top">
+                <table border="0" cellpadding="20" cellspacing="0" width="600" id="emailContainer">
+                    <tr>
+                        <td align="center" valign="top">
+                            <h3 style="height: 40px; line-height: 40px; background-color: #f56857; color: #ffffff;">There has been an exception thrown on {{ env('APP_URL', 'unknown') }}</h3>
+                            <table class="emailExceptionTable" style="text-align: left;" border="0" cellspacing="0" cellpadding="3">
                                 <tr>
-                                    <td><strong>Robot:</strong></td>
-                                    <td>{!! $agent->robot() !!}</td>
+                                    <td><strong>Environment:</strong></td>
+                                    <td>{{ env('APP_ENV', 'unknown') }}</td>
                                 </tr>
-                            @endif
-                        @endif
-                        @if($user)
-                            <tr>
-                                <td><strong>User:</strong></td>
-                                <td><strong>{{ $user->name ?? ($user->username ?? null) }} <a href="mailto:{{ $user->email }}" target="_blank">{{ $user->email }}</a></strong></td>
-                            </tr>
-                        @endif
-                            <tr>
-                                <td><strong>Exception Class:</strong></td>
-                                <td>{{ get_class($exception) }}</td>
-                            </tr>
-                            <tr>
-                                <td><strong>Exception Message:</strong></td>
-                                <td>{{ $exception->getMessage() }}</td>
-                            </tr>
-                            <tr>
-                                <td><strong>Exception Code:</strong></td>
-                                <td>{{ $exception->getCode() }}</td>
-                            </tr>
-                        </table>
-                        <hr style="color: #f6f6f6;">
-                        <table align="center" style="text-align: center;" border="0" cellspacing="0" cellpadding="0">
-                            <tr>
-                                <td>In {{ $exception->getFile() }} on line {{  $exception->getLine() }}</td>
-                            </tr>
-                        </table>
-                        <hr style="color: #f6f6f6;">
-                        <table align="center" style="text-align: center;" border="0" cellspacing="0" cellpadding="0">
-                            <tr>
-                                <td><strong>Stack Trace:</strong></td>
-                            </tr>
-                            <tr><td align="left" style="text-align: left;">{!! nl2br($exception->getTraceAsString()) !!}</td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
-        </td>
-    </tr>
-</table>
+                                @if($request)
+                                    <tr>
+                                        <td><strong>Exception Url:</strong></td>
+                                        <td>{!! $request->fullUrl() ?? null !!}</td>
+                                    </tr>
+                                    <tr>
+                                        <td><strong>Remote Host:</strong></td>
+                                        <td>{!! $request->ip() ?? null !!}</td>
+                                    </tr>
+
+                                    @if($request->input())
+                                        <tr>
+                                            <td><strong>Request:</strong></td>
+                                            <td><pre>{!! print_r($request->input()) !!}</pre></td>
+                                        </tr>
+                                    @endif
+                                @endif
+
+                                @isset($agent)
+                                    <tr>
+                                        <td><strong>UserAgent:</strong></td>
+                                        <td>{!! $agent->getUserAgent() !!}</td>
+                                    </tr>
+                                    @if($agent->isRobot())
+                                        <tr>
+                                            <td><strong>Robot:</strong></td>
+                                            <td>{!! $agent->robot() !!}</td>
+                                        </tr>
+                                    @endif
+                                @endisset
+                                @isset($user)
+                                    <tr>
+                                        <td><strong>User:</strong></td>
+                                        <td><strong>{{ $user->name ?? ($user->username ?? null) }} <a href="mailto:{{ $user->email }}" target="_blank">{{ $user->email }}</a></strong></td>
+                                    </tr>
+                                @endisset
+                                <tr>
+                                    <td><strong>Exception Class:</strong></td>
+                                    <td>{{ get_class($exception) }}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Exception Message:</strong></td>
+                                    <td>{{ $exception->getMessage() }}</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Exception Code:</strong></td>
+                                    <td>{{ $exception->getCode() }}</td>
+                                </tr>
+                            </table>
+                            <hr style="color: #f6f6f6;">
+                            <table align="center" style="text-align: center;" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td>In {{ $exception->getFile() }} on line {{  $exception->getLine() }}</td>
+                                </tr>
+                            </table>
+                            <hr style="color: #f6f6f6;">
+                            <table align="center" style="text-align: center;" border="0" cellspacing="0" cellpadding="0">
+                                <tr>
+                                    <td><strong>Stack Trace:</strong></td>
+                                </tr>
+                                <tr><td align="left" style="text-align: left;">{!! nl2br($exception->getTraceAsString()) !!}</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+@else
+    <h3>Sorry, no request data given</h3>
+@endisset
 </body>
 </html>
